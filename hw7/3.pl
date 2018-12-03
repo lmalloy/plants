045036021fceb62e9b1032dd2c7d6a7fef2e36b8
Luke Malloy <ldm437@mst.edu>
2018-12-03 11:54:09 -0600

    adding hw8 files

--- hw7/3.pl
+++ hw7/3.pl
@@ -1,23 +1,93 @@
-access(X) :- weapon_access(X), key_access(X), crime_access(X)).
+access(X) :- weapon_access(X), key_access(X), crime_access(X).
 
-weapon_access(X) :- stay(X, Thursday, lab).
+weapon_access(X) :- stay(X, thursday, lab).
+weapon_access(X) :- stay(X, wednesday, csOffice).
+weapon_access(X) :- stay(X, thursday, bookstore).
+weapon_access(X) :- stay(X, wednesday, bookstore).
 
-weapon_access(X) :- stay(X, Wednesday, cs_office).
+key_access(X) :- stay(X, monday, csOffice).
+key_access(X) :- stay(X, tuesday, lab).
 
-weapon_access(X) :- stay(X, Thursday, bookstore).
+key_access(davis).
 
-weapon_access(X) :- stay(X, Wednesday, bookstore).
+crime_access(X) :- stay(X, thursday, bookstore).
+crime_access(X) :- stay(X, friday, bookstore).
 
-key_access(X) :- stay(X, Monday, cs_office).
+stay(fu, monday, csOffice).
+stay(fu, tuesday, csOffice).
+stay(fu, wednesday, lab).
+stay(fu, thursday, csOffice).
+stay(fu, friday, bookstore).
 
-key_access(X) :- stay(X, Tuesday, lab).
+stay(mcmillin, monday, lab).
+stay(mcmillin, tuesday, lab).
+stay(mcmillin, wednesday, lab).
+stay(mcmillin, thursday, csOffice).
+stay(mcmillin, friday, bookstore).
 
-key_access(david).
+stay(das, monday, bookstore).
+stay(das, tuesday, lab).
+stay(das, wednesday, bookstore).
+stay(das, thursday, bookstore).
+stay(das, friday, bookstore).
 
-crime_access(X) :- stay(X, Thursday, bookstore).
+stay(davis, monday, bookstore).
+stay(davis, tuesday, lab).
+stay(davis, wednesday, lab).
+stay(davis, thursday, csOffice).
+stay(davis, friday, bookstore).
 
-crime_access(X) :- stay(X, Friday, bookstore).
+stay(sabharwal, monday, bookstore).
+stay(sabharwal, tuesday, bookstore).
+stay(sabharwal, wednesday, csOffice).
+stay(sabharwal, thursday, bookstore).
+stay(sabharwal, friday, bookstore).
 
+stay(markowsky, monday, bookstore).
+stay(markowsky, tuesday, lab).
+stay(markowsky, wednesday, lab).
+stay(markowsky, thursday, bookstore).
+stay(markowsky, friday, bookstore).
 
+stay(price, monday, csOffice).
+stay(price, tuesday, csOffice).
+stay(price, wednesday, lab).
+stay(price, thursday, csOffice).
+stay(price, friday, bookstore).
 
+stay(tauritz, monday, bookstore).
+stay(tauritz, tuesday, csOffice).
+stay(tauritz, wednesday, csOffice).
+stay(tauritz, thursday, csOffice).
+stay(tauritz, friday, bookstore).
+
+motive(X, jealous):- victim(Y), jealous(X,Y).
+motive(X, insane):-insane(X).
+motive(X, poor):-poor(X).
+
+jealous(X,Y):- friends_with(X,Z), friends_with(Y,Z).
+
+victim(leopold).
+
+insane(sabharwal).
+insane(tauritz).
+
+poor(davis).
+poor(fu).
+poor(morales).
+poor(tauritz).
+
+friends_with(X,Y):-friends(X,Y).
+friends_with(X,Y):-friends(Y,X).
+
+friends(leopold,price).
+friends(price, mcmillin).
+friends(leopold, fu).
+friends(fu, markowsky).
+friends(markowsky, das).
+friends(das, tauritz).
+friends(sabharwal, tauritz).
+friends(sabharwal, mcmillin).
+
+murderer(X, Y):- motive(X, Y), access(X).
 
---
+++ hw8/malloyl.sh
@@ -0,0 +1,11 @@
+# @author: Luke Malloy
+# @description: CS3500 Homework 8: Bash Script - Autograding
+
+# Unzip the files
+unzip sampleInput.zip -d sampleInput
+unzip sampleOutput.zip -d sampleOutput
+unzip submissions.zip -d submissions
+
+# Process every entry in the submissions directory 
+
+
